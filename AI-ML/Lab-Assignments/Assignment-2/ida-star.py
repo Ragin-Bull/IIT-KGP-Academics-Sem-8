dd3bccf471a881a6c6058f0c7583d23c4da35eae
Shatansh <shatansh.iitkgp@gmail.com>
2024-01-22 13:33:17 +0530

    Added ida-star

---
+++ AI-ML/Lab-Assignments/Assignment-2/ida-star.py
@@ -0,0 +1,49 @@
+inf = 999999
+
+def ida_star(start, goal):
+    threshold = heuristic(start, goal)
+
+    while True:
+        visited = set()
+        res, currentCost = DLS(start, goal, threshold, visited)
+
+        if res == "Success":
+            return currentCost  # Goal has been reached
+
+        if res == inf:
+            return "Goal is not reachable"  # Goal is not reachable
+
+        threshold = res
+
+
+def DLS(current, goal, threshold, visited):
+    currentCost = heuristic(current, goal)
+
+    if currentCost > threshold:
+        return currentCost, 0
+
+    if current == goal:
+        return "Success", 0
+
+    minCost = inf
+
+    for nhbr, weight in graph[current]:
+        if nhbr not in visited:
+            visited.add(nhbr)
+            res, pathCost = DLS(nhbr, goal, threshold, visited)
+
+            if res == "Success":
+                return "Success", pathCost + weight
+
+            if res != inf and pathCost + weight < minCost:
+                minCost = pathCost + weight
+
+
+def heuristic(current, goal):
+    heuristics = [5, 15, 2, 8, 0]
+    return heuristics[current]
+
+
+graph = [[(1, 5), (2, 3)], [(3, 7)], [(4, 2)], [(4, 8)], []]
+
+print(ida_star(0, 4))
